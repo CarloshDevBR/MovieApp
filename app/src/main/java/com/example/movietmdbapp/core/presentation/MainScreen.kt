c049eaa150e66536e365a6e355a0b8c3bf990214
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-12-11 22:31:20 -0300

    style: interface main adicionada e theme definido

---
+++ app/src/main/java/com/example/movietmdbapp/core/presentation/MainScreen.kt
@@ -0,0 +1,25 @@
+package com.example.movietmdbapp.core.presentation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.movietmdbapp.core.presentation.navigation.BottomNavigationBar
+import com.example.movietmdbapp.core.presentation.navigation.NavigationGraph
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = { BottomNavigationBar(navController = navController) }
+    ) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) {
+            NavigationGraph(navController = navController)
+        }
+    }
+}
